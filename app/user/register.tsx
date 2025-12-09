33ac330350fac803f96e4187fa9fa2f2955d89d6
Kiara Maldonado <usr4066@salesianos-lacuesta.net>
2025-12-09 14:00:43 +0000

    feat(register-screen): add form for user to input data

---
+++ app/user/register.tsx
@@ -0,0 +1,88 @@
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Alert,
+  Pressable,
+} from "react-native";
+import React, { useState } from "react";
+import typography from "../../theme/typography";
+import colors from "../../theme/color";
+import { validateEmail, validatePassword } from "../../utils/inputValidation";
+
+const Register = () => {
+  const [fullname, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pswd, setPassword] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <Text style={[typography.subheading, styles.title]}>Register</Text>
+      <View style={styles.inputField}>
+        <Text style={typography.label}>Full Name: </Text>
+        <TextInput
+          placeholder="Pepe Benavente"
+          style={styles.input}
+          value={fullname}
+          onChangeText={setFullName}
+        />
+      </View>
+      <View style={styles.inputField}>
+        <Text style={typography.label}>Email address: </Text>
+        <TextInput
+          placeholder="pepe@email.es"
+          keyboardType="email-address"
+          style={styles.input}
+          value={email}
+          onChangeText={setEmail}
+        />
+      </View>
+      <View style={styles.inputField}>
+        <Text style={typography.label}>Password: </Text>
+        <TextInput
+          placeholder="Password"
+          secureTextEntry
+          style={styles.input}
+          value={pswd}
+          onChangeText={setPassword}
+        />
+      </View>
+      <Pressable onPress={submitForm} style={styles.button}>
+        <Text style={typography.button}>Register Now!</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default Register;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
+    backgroundColor: colors.secondaryBackground,
+    flex: 1,
+    justifyContent: "center",
+    paddingBottom: 100,
+  },
+  title: {
+    textAlign: "center",
+  },
+  inputField: {
+    marginBottom: 10,
+  },
+  input: {
+    backgroundColor: colors.secondaryText,
+    borderRadius: 10,
+    width: "100%",
+    padding: 10,
+  },
+  button: {
+    backgroundColor: colors.buttonBackground,
+    padding: 10,
+    borderRadius: 15,
+    alignItems: "center",
+    width: 150,
+    alignSelf: "center",
+  },
+});
