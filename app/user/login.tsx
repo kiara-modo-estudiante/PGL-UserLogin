a60a59b859f7c3f7c93fefe5952a5dae062cbcf3
Kiara Maldonado <usr4066@salesianos-lacuesta.net>
2025-12-10 09:26:59 +0000

    feat(login-screen): implement email and password input fields with styling

--- app/user/login.tsx
+++ app/user/login.tsx
@@ -1,14 +1,70 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import colors from "../../theme/color";
+import typography from "../../theme/typography";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [pswd, setPassword] = useState("");
+
   return (
-    <View>
-      <Text>login</Text>
+    <View style={styles.container}>
+      <Text style={[typography.subheading, styles.title]}>Log In</Text>
+      <View style={styles.inputField}>
+        <Text style={typography.label}>Email address: </Text>
+        <TextInput
+          placeholder="pepe@email.es"
+          keyboardType="email-address"
+          style={styles.input}
+          value={email}
+          onChangeText={setEmail}
+        />
+      </View>
+      <View style={styles.inputField}>
+        <Text style={typography.label}>Password: </Text>
+        <TextInput
+          placeholder="Password"
+          secureTextEntry
+          style={styles.input}
+          value={pswd}
+          onChangeText={setPassword}
+        />
+      </View>
+      {/* <Pressable onPress={submitForm} style={styles.button}>
+        <Text style={typography.button}>Register Now!</Text>
+      </Pressable> */}
     </View>
   );
 };
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
+    backgroundColor: colors.secondaryBackground,
+    flex: 1,
+    justifyContent: "center",
+    paddingBottom: 100,
+  },
+  title: {
+    textAlign: "center",
+  },
+  inputField: {
+    marginBottom: 10,
+  },
+  input: {
+    backgroundColor: colors.secondaryText,
+    borderRadius: 10,
+    width: "100%",
+    padding: 10,
+  },
+  button: {
+    backgroundColor: colors.buttonBackground,
+    padding: 10,
+    borderRadius: 15,
+    alignItems: "center",
+    width: 150,
+    alignSelf: "center",
+  },
+});
