7b91870df53f087ed680226db071034abde43ed8
Kiara Maldonado <usr4066@salesianos-lacuesta.net>
2025-12-13 08:55:32 +0000

    refactor(drawer): modify layout and welcome screen for navigation

---
+++ app/(drawer)/_layout.tsx
@@ -0,0 +1,29 @@
+import { Drawer } from "expo-router/drawer";
+
+export default function Layout() {
+  return (
+    <Drawer>
+      <Drawer.Screen
+        name="welcome"
+        options={{
+          drawerLabel: "Home",
+          title: "Home",
+        }}
+      />
+      <Drawer.Screen
+        name="portfolio"
+        options={{
+          drawerLabel: "Portfolio",
+          title: "Portfolio",
+        }}
+      />
+      <Drawer.Screen
+        name="books"
+        options={{
+          drawerLabel: "Libros",
+          title: "Libros",
+        }}
+      />
+    </Drawer>
+  );
+}
